cde81d14c4c73c040c7c3d138077e7860107216b
Dan Kolbman <cisadmin@wpa082044.wireless.mcgill.ca>
2014-02-22 17:22:16 -0500

    Update styling, add js

---
+++ client/js/render.js
@@ -0,0 +1,12 @@
+/*
+ * Canvas functions to handle the rendering of bitmaps.
+ */
+
+/* Initialize canvas objects */
+window.onload = function () {
+  var c = document.getElementById('game');
+  var ctx = c.getContext('2d');
+  ctx.fillStyle = '#f00";
+  ctx.fillRect(5,5,95,95);
+  
+}
