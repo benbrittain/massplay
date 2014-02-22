0791b5a140d0fe4301336044ab40c0effa6e96a6
Lee Avital <leeavital@gmail.com>
2014-02-22 15:15:54 -0500

    Use websockets transport ONLY in client.js

--- client/echoserver/client.js
+++ client/echoserver/client.js
@@ -1,8 +1,9 @@
 var io = require( 'socket.io-client' );
 
 
+console.log( io );
 
-var socket = io.connect('http://localhost:9002');
+var socket = io.connect('http://localhost:9002', {transports: ['websocket']} );
 socket.on( 'default' , function(data){
   console.log( data );
 
