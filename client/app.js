4fa78d9f7b96a0a1e5ef25a52f54955820d06432
Lee Avital <leeavital@gmail.com>
2014-02-22 18:13:58 -0500

    Add more broken websocket stuff

--- client/app.js
+++ client/app.js
@@ -1,7 +1,25 @@
 var ws = new WebSocket( 'ws://localhost:9002' );
+ws.binaryType = 'arraybuffer';
+
 
 ws.onmessage = function( message ){
+
   console.log( message.data );
+
+  if( message.data instanceof ArrayBuffer ){
+    console.log( 'got an arraybuffer' );
+    var bytes = new Uint8Array( message.data );
+    var image = context.createImageData(canvas.width, canvas.height);
+    for (var i=0; i<image.length; i++) {
+          image[i] = bytes[i];
+    }
+      console.log( image );
+     // context.drawImage(image );
+
+      $('body').append( 
+        $('<img/>').attr( 'src', image.toString('base64') )
+      );
+    }
 };
 
 ws.onopen = function( ){
--- client/echoserver/main.js
+++ client/echoserver/main.js
@@ -1,8 +1,37 @@
 var WebSocketServer = require('ws').Server
+var fs = require( 'fs' );
+var mime = require( 'mime' );
+
+
+
+
+
+function makeInt32Array( buff ){
+  var arr = new Int32Array( buff.length );
+  for( var i = 0; i< buff.length; i++ ){
+    arr.set( i, buff.get( i ) );
+  }
+  return arr;
+}
+
+
 var wss = new WebSocketServer({port: 9002});
+
+
+
+
+
 wss.on('connection', function(ws) {
+
+
+  fs.readFile( 'echoserver/duck.bmp', function(err, data) {
+    ws.send(data);
+
+  });
+
+
+
   ws.on('message', function(message) {
-    console.log('received: %s', message);
     ws.send( 'message' );
   });
   ws.send('something');
