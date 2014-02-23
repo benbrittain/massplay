a6a4d48f7131c4b0d0c850e68642cbc7af521f72
Lee Avital <leeavital@gmail.com>
2014-02-23 02:35:38 -0500

    newfile route

--- client/site.js
+++ client/site.js
@@ -3,6 +3,11 @@ var express = require( 'express' );
 var app = express();
 
 
+app.get( '/newgame', function( req, res ){
+  res.send( 'hallo world' );
+});
+
+
 app.get( '*', function(req, res) {
 
     var path = req.params.join('/').replace( /^\//, '' );
