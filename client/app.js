78eb4e53eaf1c1a3b2e8a46c43b25158a7654c42
Lee Avital <leeavital@gmail.com>
2014-02-23 01:16:36 -0500

    Got rid of commented out, dead code in app.js

--- client/app.js
+++ client/app.js
@@ -14,20 +14,8 @@ function _arrayBufferToBase64( buffer ) {
 
 ws.onmessage = function( message ){
       if(1) {
-//        var b = _arrayBufferToBase64( message.data );
-
-
 
       console.log( 'got a frame' );
-      // var id = window.context.createImageData( 240, 160);
-      // for( var i = 0, j = 0; i < id.data.length; i+=4, j+= 3   ){
-      //   id.data[i] = arr[j];
-      //   id.data[i+1] = arr[j + 1];
-      //   id.data[i+2] = arr[j + 2];
-      //   id.data[i+3] = 255;
-      // }
-
-      // context.putImageData( id, 0, 0 );
 
       var img = document.createElement( 'img' );
       img.src = 'data:image/bmp;base64,' + message.data;
@@ -39,19 +27,6 @@ ws.onmessage = function( message ){
         console.log( 'loaded' );
         context.drawImage( img, 0, 0 );
       }
-  // try{
-  //   console.log( message.data );
-  //   var bytes = new Uint8Array( message.data );
-  //   console.log( bytes );
-  //   var image = context.createImageData(500, 546);
-  //   for (var i=0; i<image.length; i++) {
-  //         image[i] = bytes[i];
-  //   }
-
-  //   }catch( e ){
-  //     alert( e );
-  //     alert( e.getMessage() );
-  //   }
   }
 };
 
